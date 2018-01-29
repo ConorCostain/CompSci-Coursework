d6bb91c834280fbfdc50acee5ed58b93af1b2cb8
Conor Costain <conorcostain@gmail.com>
2018-01-29 12:58:01 +0000

    Created Main Menu

---
+++ CompSci_Coursework/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void PlayButton()
+	{
+		Debug.Log("Play Button Pressed");
+	}
+
+	public void LevelsButton()
+	{
+		Debug.Log("Levels Button Pressed");
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
---
+++ CompSci_Coursework/Assets/Scripts/PlaySessionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaySessionManager : MonoBehaviour {
+
+	public static PlaySessionManager ins;
+
+	void Awake()	// Runs when script is loaded, before game runs
+	{
+		// Singleton Pattern to have a single instance of the PlaySession manager
+		if(ins == null)	//Checks whether there is already an Instance,
+		{
+			ins = this;	//If there is no instance sets itself to be the instance
+			DontDestroyOnLoad(gameObject);	//Makes it so the object will persist through scenes
+		}
+		else if(ins != this)	//if the instance is set to something other than this object
+		{
+			Destroy(gameObject);	//destroy this object as there is already a different instance
+			return;					// returns to prevent running any more code in this method
+									// as it can take a short time for the object to be destroyed
+		}
+	}
+
+
+}
